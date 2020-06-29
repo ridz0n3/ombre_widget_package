507173eef783ab058aa78187e3c339313868ee7c
ridzuan othman <ridz0n91@gmail.com>
2020-06-29 13:58:30 +0800

    resize button

--- lib/button/single_button.dart
+++ lib/button/single_button.dart
@@ -16,16 +16,16 @@ class SingleButton extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: ScreenUtil().setHeight(40),
+      height: ScreenUtil().setHeight(57),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(ScreenUtil().setHeight(40))
+          borderRadius: BorderRadius.circular(ScreenUtil().setHeight(64))
       ),
       child: Material(
         elevation: ScreenUtil().setHeight(2),
         shadowColor: Color(hexStringToHexInt('#40BB2034')),
-        borderRadius: BorderRadius.circular(ScreenUtil().setHeight(40)),
+        borderRadius: BorderRadius.circular(ScreenUtil().setHeight(64)),
         child: ClipRRect(
-          borderRadius: BorderRadius.circular(ScreenUtil().setHeight(40)),
+          borderRadius: BorderRadius.circular(ScreenUtil().setHeight(64)),
           child: CupertinoButton(
             padding: const EdgeInsets.all(0.0),
             child: Stack(
@@ -33,11 +33,11 @@ class SingleButton extends StatelessWidget{
               children: <Widget>[
                 Container(
                   decoration: BoxDecoration(
-                    border: onPressed == null ? Border.all(color: Color(hexStringToHexInt('#a1a1a1'))) : Border.all(color: Color(hexStringToHexInt('#D67154'))),
-                    borderRadius: BorderRadius.all(Radius.circular(ScreenUtil().setHeight(40))),
+                    //border: onPressed == null ? Border.all(color: Color(hexStringToHexInt('#a1a1a1'))) : Border.all(color: Color(hexStringToHexInt('#D67154'))),
+                    borderRadius: BorderRadius.all(Radius.circular(ScreenUtil().setHeight(64))),
                     color: Color(hexStringToHexInt('#ffffff')),
                     gradient: isFill ? LinearGradient(
-                      begin: Alignment.topLeft,
+                      begin: Alignment.topCenter,
                       end: Alignment.bottomCenter,
                       stops: [0.0, 0.17, 0.47, 0.69, 1.0],
                       colors: [
