9ae5e6098e88eb4fff041197f68bef56345a5f95
ridzuan othman <ridz0n91@gmail.com>
2020-06-29 15:07:40 +0800

    add header and subheader

---
+++ lib/header/header_text.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/screenutil.dart';
+import 'package:ombre_widget_package/helper/utils.dart';
+
+class HeaderText extends StatelessWidget{
+  final String title;
+  double top;
+  HeaderText({this.title, this.top = 32});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        top: ScreenUtil().setHeight(top),
+        left: ScreenUtil().setWidth(33),
+        right: ScreenUtil().setWidth(33),
+      ),
+      child: Hero(
+        tag: 'subheader',
+        child: Container(
+          child: Text(
+            '$title',
+            textAlign: TextAlign.center,
+            style: getCustomFont(Color(hexStringToHexInt('#FDFFFC')), 29, 'PlayfairDisplay-Bold'),
+          ),
+        ),
+      ),
+    );
+  }
+}
