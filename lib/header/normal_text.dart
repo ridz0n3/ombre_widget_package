0a3dc0b422959b0f8b7ee16b39a92cc937515ac8
ridzuan othman <ridz0n91@gmail.com>
2020-07-06 11:20:36 +0800

    add align text

--- lib/header/normal_text.dart
+++ lib/header/normal_text.dart
@@ -6,13 +6,17 @@ class NormalText extends StatelessWidget{
   final String text;
   double fontSize;
   String colorHex;
-  NormalText({this.text, this.fontSize = 11, this.colorHex = '#FFFCF2'});
+  AlignmentDirectional align;
+  NormalText({this.text, this.fontSize = 11, this.colorHex = '#FFFCF2', this.align = AlignmentDirectional.centerStart});
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: getCustomFont(Color(hexStringToHexInt(colorHex)), fontSize, 'Poppins-Regular'),
+    return Align(
+      alignment: align,
+      child: Text(
+        text,
+        style: getCustomFont(Color(hexStringToHexInt(colorHex)), fontSize, 'Poppins-Regular'),
+      ),
     );
   }
 }
