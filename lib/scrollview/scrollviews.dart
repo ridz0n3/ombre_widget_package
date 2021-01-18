f6a31644e4d738e5c91449a45cf32d3563e2c846
iwang <ridz0n391@gmail.com>
2021-01-18 12:17:52 +0800

    adding horizontal scrollview

--- lib/scrollview/scrollviews.dart
+++ lib/scrollview/scrollviews.dart
@@ -3,23 +3,20 @@ import 'package:flutter/material.dart';
 class ScrollViews extends StatelessWidget {
 
   List<Widget> children;
+  Axis scrollDirection;
   ScrollController scrollController;
-  ScrollViews({List<Widget> children, ScrollController scrollController}) : this.children = children, this.scrollController = scrollController;
+  ScrollViews({this.children, this.scrollController, this.scrollDirection = Axis.vertical});
 
   @override
   Widget build(BuildContext context) {
     return new LayoutBuilder(
-      builder:
-          (BuildContext context, BoxConstraints viewportConstraints) {
+      builder: (BuildContext context, BoxConstraints viewportConstraints) {
         return SingleChildScrollView(
           controller: scrollController,
+          scrollDirection: scrollDirection,
           child: ConstrainedBox(
-            constraints:
-            BoxConstraints(minHeight: viewportConstraints.maxHeight),
-            child: Column(children:
-            // remaining stuffs
-            children
-            ),
+            constraints: BoxConstraints(minHeight: viewportConstraints.maxHeight),
+            child: scrollDirection == Axis.horizontal ? Row(children: children) : Column(children: children),
           ),
         );
       },
